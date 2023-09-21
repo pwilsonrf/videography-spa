ec8eb8bcdad7d3201599d764897594c189fe4e1d
Wilson R <47545657+pwilsonrf@users.noreply.github.com>
2023-09-20 22:16:09 -0700

    completed Works section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/header/Header";
 import css from './styles/app.module.scss'
 import Hero from "./components/hero/hero";
 import Expertise from "./components/expertise/expertise"
+import Works from "./components/works/Works";
 
 const App = () => {
   return <div className={`bg-primary ${css.container}`}>
     <Header />
     <Hero />
     <Expertise />
+    <Works />
   </div>
 };
 
